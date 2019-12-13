f539e50cfdacbd54fef74a036f904a4da21d0d2c
Denis <denisbrd8@gmail.com>
2019-12-13 22:59:21 +0700

    свойства

--- ex02/src/main/java/ru/chaban/spring/Example.java
+++ ex02/src/main/java/ru/chaban/spring/Example.java
@@ -1,17 +1,34 @@
 package ru.chaban.spring;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import ru.chaban.spring.service.TestStudent;
 import ru.chaban.spring.service.TestStudentImpl;
 
 @ComponentScan
 @Configuration
+@PropertySource("classpath:application.properties")
+
 public class Example {
+
+    private int minimumPositiveQuestionsForPassExam;
+
+    public Example( @Value("${db.minimumPositivQuestionsForPassExam}") int minimumPositiveQuestionsForPassExam) {
+        System.out.println("!!!!!!! " + minimumPositiveQuestionsForPassExam);
+        this.minimumPositiveQuestionsForPassExam = minimumPositiveQuestionsForPassExam;
+    }
+
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Example.class);
         TestStudent testStudent = context.getBean(TestStudentImpl.class);
+
+
         testStudent.testStudent();
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev(){
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
