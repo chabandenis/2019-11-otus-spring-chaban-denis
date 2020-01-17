3d84d280f1a5598c829a34f9bd9fececa0ae6470
Denis <denisbrd8@gmail.com>
2020-01-17 16:14:08 +0700

    test 04v2

---
+++ ex04v2/src/main/java/ru/chaban/ex3/domain/Test.java
@@ -0,0 +1,61 @@
+package ru.chaban.ex3.domain;
+
+import org.springframework.context.MessageSource;
+
+import java.util.List;
+import java.util.Locale;
+
+/*
+    Результаты теста
+ */
+public class Test {
+    // персона
+    final private Person person;
+
+    // вопросы
+    final private Questions questions;
+
+    // правильные ответы
+    private int correctAnswers = 0;
+
+    // ответы с ошибками
+    private int incorrectAnswers = 0;
+
+    public List<Question> getQuestions() {
+        return questions.getQuestions();
+    }
+
+    // один конструктор с обязательным указанием персоны
+    public Test(Person person, Questions questions) {
+        this.person = person;
+        this.questions = questions;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public int getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public int getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
+    // принять ответ
+    public String getAnswer(Boolean isCorrect, MessageSource messageSource, Locale locale) {
+        if (isCorrect) {
+            correctAnswers++;
+            return messageSource.getMessage("answer.right", null, locale);
+        } else {
+            incorrectAnswers--;
+            return messageSource.getMessage("answer.wrong", null, locale);
+        }
+    }
+
+    // общее количество вопросов
+    public int totalQuestions() {
+        return correctAnswers + incorrectAnswers;
+    }
+}
---
+++ ex04v2/src/main/java/ru/chaban/ex3/service/GetQuestionsImpl.java
@@ -0,0 +1,105 @@
+package ru.chaban.ex3.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.chaban.ex3.Exceptions.NoFileWithQuestions;
+import ru.chaban.ex3.domain.Question;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    Сервис получения актупльного списка вопросов
+ */
+@Service
+public class GetQuestionsImpl implements GetQuestions {
+    // свойства с которыми запустилось приложение
+    private final Properties properties;
+
+    public GetQuestionsImpl(Properties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public List<Question> getQuestions() throws NoFileWithQuestions {
+
+        List<Question> questionAndAnswers = new ArrayList<>();
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(properties.getTestFileName());
+
+        if (inputStream == null) {
+            throw new NoFileWithQuestions("Файл \"" + properties.getTestFileName() + " \" вопросами не найден. Выберите существующий");
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String line;
+        int lineNumber = 0;
+        int countOfQuestions = -1;
+        int indexOfQuestion = -1;
+        int countOfAnswers = -1;
+        int indexOfAnswers = -1;
+
+        String question = "";
+
+        List<String> answers = null;
+        List<Boolean> correctAnswers = null;
+
+        boolean isQuestion = true;
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+
+                // в первой строке содежрится количество вопросов
+                if (lineNumber == 0) {
+                    countOfQuestions = Integer.parseInt(line);
+                }
+                // последующие стртоки содержат вопросы с количеством ответов и ответы
+                else {
+                    if (indexOfQuestion == -1) {
+                        indexOfQuestion++;
+                        isQuestion = true;
+                    }
+
+                    String[] splitLine = line.split(",");
+
+                    // ответы
+                    if (!isQuestion) {
+                        indexOfAnswers++;
+                        answers.add(splitLine[0].trim());
+                        correctAnswers.add(Boolean.parseBoolean(splitLine[1].trim()));
+                    }
+
+                    // вопросы
+                    if (isQuestion) {
+                        question = splitLine[0].trim();
+                        countOfAnswers = Integer.parseInt(splitLine[1].trim());
+                        isQuestion = false;
+                        answers = new ArrayList<>();
+                        correctAnswers = new ArrayList<>();
+                    }
+
+                    // сохраним
+                    if (countOfAnswers - 1 == indexOfAnswers && countOfAnswers != -1) {
+                        questionAndAnswers.add(new Question(question, answers, correctAnswers));
+                        indexOfQuestion++;
+                        isQuestion = true;
+
+                        // обнулим
+                        question = "";
+                        indexOfAnswers = -1;
+                    }
+                }
+
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return questionAndAnswers;
+    }
+}
