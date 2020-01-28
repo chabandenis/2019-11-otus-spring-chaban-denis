cf58288421fdcb5e24dc36c28e534c3adf4e6b1d
Denis <denisbrd8@gmail.com>
2020-01-28 09:40:56 +0700

    вопрос, не могу запустить скрипты sql

---
+++ ex05v3/src/main/java/ru/chaban/ex05/dao/AuthorDaoImpl.java
@@ -0,0 +1,75 @@
+package ru.chaban.ex05.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.stereotype.Repository;
+import ru.chaban.ex05.domain.Author;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings({"SqlNoDataSourceInspection", "SqlDialectInspection"})
+@Repository
+public class AuthorDaoImpl implements AuthorDao {
+    private final NamedParameterJdbcOperations jdbc;
+
+    public AuthorDaoImpl(NamedParameterJdbcOperations jdbcOperations) {
+        this.jdbc = jdbcOperations;
+    }
+
+    @Override
+    public void insert(Author author) {
+        final Map<String, Object> params = new HashMap<>(2);
+        params.put("id", author.getId());
+        params.put("name", author.getName());
+
+        jdbc.update("insert into authors(`id`, `name`) values (:id, :name) ", params);
+    }
+
+    @Override
+    public void update(Author author) {
+        final Map<String, Object> params = new HashMap<>(2);
+        params.put("id", author.getId());
+        params.put("name", author.getName());
+
+        jdbc.update("update authors set name=:name where id = :id)", params);
+    }
+
+    @Override
+    public void deleteById(long id) {
+        final Map<String, Object> params = new HashMap<>(1);
+        params.put("id", id);
+        jdbc.update(" delete from authors where id = :id", params);
+    }
+
+    @Override
+    public Author getById(long id) {
+        final Map<String, Object> params = new HashMap<>(1);
+        params.put("id", id);
+        return jdbc.queryForObject("select * from authors where id = :id ", params, new Mapper());
+    }
+
+    @Override
+    public List<Author> getAll() {
+        return jdbc.query("select * from authors", new Mapper());
+    }
+
+    @Override
+    public int count() {
+        final Map<String, Object> params = new HashMap<>(1);
+        params.put("id", 0);
+        return jdbc.queryForObject("select count(1) from authors", params, Integer.class);
+    }
+
+    private static class Mapper implements RowMapper<Author> {
+        @Override
+        public Author mapRow(ResultSet resultSet, int i) throws SQLException {
+            long id = resultSet.getLong("id");
+            String name = resultSet.getString("name");
+            return new Author(id, name);
+        }
+    }
+}
---
+++ ex05v3/src/main/java/ru/chaban/ex05/dao/BookGenresDaoImpl.java
@@ -0,0 +1,77 @@
+package ru.chaban.ex05.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.stereotype.Repository;
+import ru.chaban.ex05.domain.BookGenres;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings({"SqlNoDataSourceInspection", "SqlDialectInspection"})
+@Repository
+public class BookGenresDaoImpl implements BookGenresDao {
+    private final NamedParameterJdbcOperations jdbc;
+
+    public BookGenresDaoImpl(NamedParameterJdbcOperations jdbcOperations) {
+        this.jdbc = jdbcOperations;
+    }
+
+    @Override
+    public void insert(BookGenres bookGenres) {
+        final Map<String, Object> params = new HashMap<>(3);
+        params.put("id", bookGenres.getId());
+        params.put("book_id", bookGenres.getBookId());
+        params.put("genre_Id", bookGenres.getGenreId());
+
+        jdbc.update("insert into book_genres (id, book_Id, genre_Id) values (:id, :book_id, :genre_Id)", params);
+    }
+
+    @Override
+    public void update(BookGenres bookGenres) {
+        final Map<String, Object> params = new HashMap<>(3);
+        params.put("id", bookGenres.getId());
+        params.put("book_id", bookGenres.getBookId());
+        params.put("genre_Id", bookGenres.getGenreId());
+
+        jdbc.update("update book_genres set book_Id =:book_Id and genre_Id =:genre_Id where id = :id", params);
+    }
+
+    @Override
+    public void deleteById(long id) {
+        final Map<String, Object> params = new HashMap<>(1);
+        params.put("id", id);
+        jdbc.update("delete from book_genres where id= :id", params);
+    }
+
+    @Override
+    public BookGenres getById(long id) {
+        final Map<String, Object> params = new HashMap<>(1);
+        params.put("id", id);
+        return jdbc.queryForObject("select * from book_genres where id = :id ", params, new Mapper());
+    }
+
+    @Override
+    public List<BookGenres> getAll() {
+        return jdbc.query("select * from book_genres", new Mapper());
+    }
+
+    @Override
+    public int count() {
+        final Map<String, Object> params = new HashMap<>(1);
+        return jdbc.queryForObject("slect count(1) from book_genres", params, Integer.class);
+    }
+
+    private static class Mapper implements RowMapper<BookGenres> {
+        @Override
+        public BookGenres mapRow(ResultSet resultSet, int i) throws SQLException {
+            long id = resultSet.getLong("id");
+            long bookId = resultSet.getLong("book_Id");
+            long genreId = resultSet.getLong("genre_Id");
+            return new BookGenres(id, bookId, genreId);
+        }
+    }
+}
