b94fe9314f63701d5108ab8acac057665f9331ce
DCHaban <denisbrd8@gmail.com>
2021-01-31 21:40:53 +0700

    1

---
+++ mongo_magic/src/main/java/ru/chaban/white_magic/service/OpinionServiceCustom.java
@@ -0,0 +1,10 @@
+package ru.chaban.white_magic.service;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface OpinionServiceCustom {
+    // удалить все комментарии ко всем книгам
+    void customDelete();
+}
